7ef2bb6eb8a9dec924d77c1de63887dd59035ef8
Saad Muttahir Shah <saadmuttahirshah@gmail.com>
2025-01-25 02:48:51 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,221 @@
+// .........     PDF ONE START    ........//
+
+//Question one//
+// 1.  var username;
+
+//Question two//
+//2.  var myName='Saad Muttahir' ;
+
+//Question three//
+// var message;
+// message="helloworld";
+//  alert(message)
+
+// Question three//
+//var studentname = "Ali Khan";
+//var studentage = "17 years";
+//var partnername = "Janeman";
+//var job = "Ethical hacker";
+
+//alert(`Your name is ${studentname}`);
+//alert(`your age is ${studentage}`);
+//alert(`your life partner will be ${partnername}`)
+//alert(`you will be an ${job}`)
+
+//Question four//
+// var name = prompt(`enter your name`)
+// var childrens = prompt(`enter no. childrens `)
+// var loc = prompt(`enter geographical location`)
+// var job = prompt(`enter your job`)
+
+// alert(`${name} your childrens will be ${childrens} .and you will live in ${loc} and you will be ${job}`)
+
+// document.write(
+//     <table border="" >
+//     <tr>
+//         <th>name</th>
+//         <th>childrens</th>
+//         <th>loc </th>
+//         <th>job</th>
+
+//     </tr>
+//     <tr>
+//         <td>${name}</td>
+//         <td>${childrens}</td>
+//         <td>${loc}</td>
+//         <td>${job}</td>
+//     </tr>  
+//     </table>
+    
+// )
+
+// question five //
+// var eat = "pizza \n pizz \n piz \n pi \n p"
+// alert (eat)
+
+// question seven //
+// var email = "abc@gmail.com"
+// alert (`my email is ${email}`  )
+
+// question eight//
+// var book = "A smarter way to learn java script"
+// alert(`I am trying to learn from the book ${book}`)
+
+// question nine //
+// var name  = "my name is ali khan "
+// var age = "I am 17 years old."
+// var study = " i study in smit"
+// var course = " I study mobile and  cloud computing modole classes"
+
+// document . write (`${name} \n ${age} \n ${study} \n ${course}` )
+
+// question ten//
+// document.write (`Yay! I can write HTML content through Javascript` )
+
+      //  .........  PDF ONE END  ..........//
+
+
+
+    //   .............PDF TWO STARTS   ...........//
+    
+    
+    //  questiuon one //
+    // var age = "18"
+    // alert (`I am ${age} years old`)
+
+    // question two //
+    
+    //  var visitcount = 20
+    //  visitcount++;
+     
+    // alert (`you have visited this site ${visitcount} time(s)`)
+
+    // question three //
+    // var birthday = "2006"
+    // document.write (`my birthday year is ${birthday} <br>` )
+    // document.write(`data type pf my declared variable is number`)
+   
+
+    // question four //
+    // var visit="jhon doe"
+    // var product="5 T shirt "
+    // var quantity = "220"
+
+    // document .write (`${visit} ordered ${product} on ${quantity} clothing store`)
+
+
+        // .............. PDF TWO END ..........//
+
+
+              //............ PDF THREE START  ...........//
+        
+        // question one //
+        // var name="saad ", id = "4748" , cnic = "42501 1234573123"
+
+        // question two //
+        // legal variables
+        // 1. var 1num =1
+        // 2. var @name = 1
+        // 3. var var =1
+        // 4. var function = 2
+        // 5. var let =7
+
+
+        // // legall variables//
+        // 1. var name =12 
+        // 2. var userage =96
+        // 3. var $amount =2000
+        // 4. var _totalprice =96000
+        // 5. var name_123 = 7ysj
+
+        // question three//
+        //  var name = "saad"
+        //  alert ( )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          //  ..................PDF FOUR START ................//
+
+            // question one //
+            // var num1=13
+            // var num2 = 9
+            // var eq = num1+num2
+            // alert(`sum of ${num1} and ${num2} is ${eq}`)
+
+
+            // question two  //
+            // var num1=13
+            // var num2 = 9
+            // var eq = num1-num2
+            // alert(`subtraction  of ${num1} and ${num2} is ${eq}`)
+
+
+            // question three //
+            // var num1=13
+            // var num2 = 9
+            // var eq = num1*num2
+            // alert(`multiplication of ${num1} and ${num2} is ${eq}`)
+
+
+          
+            // question four //
+            // var num1=13
+            // var num2 = 9
+            // var eq = num1/num2
+            // alert(`division of ${num1} and ${num2} is ${eq}`)
+
+
+            // question five //
+            // var num1=13
+            // var num2 = 9
+            // var eq = num1%num2
+            // alert(`remainder of ${num1} and ${num2} is ${eq}`)
+
+            // question six //
+            // var value ;
+            // var init=3
+            // var increment =3
+            // ++increment;
+            // var add=init+ increment
+            //  decrement=init + increment
+            // --decrement;
+            // var eq =add%increment
+            // document.write(`value after variable decleration is ${value} 
+            //    initial value : ${init}
+            //     <br> value after increment is ${increment}
+            //       <br>value after addition is ${add} 
+            //      <br> value after decrement is ${add--}
+            //     <br> the remainder is ${eq}`)
+
+            // question seven //
+            // var price=600
+            // var sales=5
+            // var eq = price*sales
+            // document.write(`Total cost to buy ${sales} ticket to a movie is ${eq}PKR` )
+                 
+
+            // question eight //
+            // var num1=4;
+
+            // var multiply=prompt("enter a number");
+            // multiply = Number(multiply);
+            // var eq = num1*multiply;
+            // document.write (eq);
+
+            // var num1=4
+            // document.write(`4 x 1 = ${num1 * 1}<br>4 x 2= ${num1 * 2} <br> 4 x 3 = ${num1 * 3} 
+            //    <br> 4 x 4 = ${num1 * 4}  <br> 4 x 5 = ${num1 * 5}  <br> 4 x 6 = ${num1 * 6}  <br> 4 x 7 = ${num1 * 7} 
+            //     <br> 4 x 8 = ${num1 * 8}  <br> 4 x 9 = ${num1 * 9}  <br> 4 x 10 = ${num1 * 10}  `)
